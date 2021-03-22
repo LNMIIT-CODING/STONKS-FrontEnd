ce5c1f054e5f28a93a0d792bb0c0cfe5e020e73b
OmGuptaIND <omgupta0720@gmail.com>
2021-03-22 22:12:12 +0530

    Issue #7: Prettier Action Added

--- src/components/Views/DashBoard/DashBoard.jsx
+++ src/components/Views/DashBoard/DashBoard.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import './DashBoard.css';
 
 //Components;
-
-
-
+import Header from '../../../partials/Header/Header';
 
 
 // This component is where all the applcation joins to one place
 export default function DashBoard() {
     return (
         <div>
-            <p>This is dash Board</p>
+            <p>This is Dash Board</p>
         </div>
     )
 }
