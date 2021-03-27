8a590135c07142ffaee409d0054f8190e05c9bdb
OmGuptaIND <omgupta0720@gmail.com>
2021-03-27 11:06:48 +0530

    Issue #21: Structured Repo and cleared some Legacy Code

---
+++ src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SideBar.css';
+
+//Components;
+
+
+export default function SideBar() {
+    return (
+        <div>
+            <p>This is Sibe Bar..</p>
+        </div>
+    )
+}
--- src/Views/DashBoard/DashBoard.jsx
+++ src/Views/DashBoard/DashBoard.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import './DashBoard.css';
 
 //Components;
-
-
-
-
+import DashCard from '../../Components/DashCard/DashCard';
+import SideBar from '../../Components/SideBar/SideBar';
 
 // This component is where all the applcation joins to one place
 export default function DashBoard() {
     return (
-        <div>
-            <p>This is dash Board</p>
+        <div className = 'dash' >
+            <SideBar />
+            <DashCard />
         </div>
     )
 }
