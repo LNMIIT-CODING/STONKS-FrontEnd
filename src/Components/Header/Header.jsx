994f855349cd591cafcd3fbfd622ce1a15c703fe
Om Gupta <68691016+OmGuptaIND@users.noreply.github.com>
2021-03-28 12:44:24 +0530

    Issue #14: Added Navbar Component (#36)

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,44 @@
-import './Header.css';
+import React, {useState} from 'react';
+import "./Header.css";
 
 //Components;
-
+import Logo from "../../assets/images/logo.png";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <div>
-            <p>This is header</p>
+    const location = useLocation();
+    const [links, setLinks] = useState([
+        {
+            _id:Math.random().toString(),
+            title:"DashBoard",
+            link:"/",
+        },
+        {
+            _id:Math.random().toString(),
+            title:"About",
+            link:"/about",
+        },
+        {
+            _id:Math.random().toString(),
+            title:"Contact",
+            link:"/contact",
+        },
+    ])
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="logo">
+          <img src={Logo} alt="err" />
+          <p>STONKS</p>
         </div>
-    )
+      </Link>
+      <div className="header-options">
+          {links?.map(({_id, title, link})=>(
+              <Link to = {link}>
+                  <p className = {`${location.pathname === link && 'active'}`} >{title}</p>
+              </Link>
+          ))}
+      </div>
+    </div>
+  );
 }
